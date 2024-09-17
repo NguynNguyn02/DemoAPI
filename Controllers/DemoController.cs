3c7e2b5f55df4577f456b2e23b62439b1585c139
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-09-17 17:26:46 +0700

    Add project files.

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webdemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class DemoController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public DemoController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        // GET api/html?url=https://example.com
+        [HttpGet]
+        public async Task<IActionResult> GetPageSource([FromQuery] string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return BadRequest("NULL");
+            }
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string pageSource = await response.Content.ReadAsStringAsync();
+                return Ok(pageSource);
+            }
+            catch (HttpRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Demo.cs
@@ -0,0 +1,9 @@
+﻿namespace Webdemo
+{
+    public class Demo
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
